687dbb2b2f25027eb0e1031be8eeb0ef751e42db
mrtong <yanjun.tong@gmail.com>
2017-04-02 18:12:58 +1000

    The order of the method filter really matters

---
+++ src/main/java/com/foo/stream/TheOrderOfFilterMatters.java
@@ -0,0 +1,47 @@
+package com.foo.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * Created by yanjuntong on 2/04/17.
+ */
+public class TheOrderOfFilterMatters {
+    public TheOrderOfFilterMatters(){
+        bothMapAndFilterCalledFiveTimes();
+        System.out.println("==============================================================");
+        mapCallOnce();
+    }
+
+    private void bothMapAndFilterCalledFiveTimes(){
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .map(s -> {
+                    System.out.println("map: " + s);
+                    return s.toUpperCase();
+                })
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("A");
+                })
+                .forEach(s -> System.out.println("foreach: " + s));
+
+    }
+
+    private void mapCallOnce(){
+        Stream.of("d2", "a2", "b1", "b3", "c")
+                .filter(s -> {
+                    System.out.println("filter: " + s);
+                    return s.startsWith("a");
+                })
+                .map(s -> {
+                            System.out.println("map: " + s);
+                            return s.toUpperCase();
+                        }
+                )
+                .forEach(s -> System.out.println("foreach: " + s));
+
+    }
+
+    public static void main(String ...a){
+        new TheOrderOfFilterMatters();
+    }
+}
