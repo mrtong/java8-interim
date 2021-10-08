c059a6e112d3e42b0bd2a86eefc2f98ccf7d6fd2
mrtong <yanjun.tong@gmail.com>
2021-10-08 17:39:23 +1100

    add more IntStream features

--- src/main/java/com/foo/stream/IntStreamSample.java
+++ src/main/java/com/foo/stream/IntStreamSample.java
@@ -6,25 +6,41 @@ import java.util.stream.IntStream;
 
 public class IntStreamSample {
     public IntStreamSample() {
-        IntStream intStream = IntStream.of(1,2,3);
+        IntStream intStream = IntStream.of(1, 2, 3);
 
         System.out.println(this.getSum(intStream));
-        intStream = IntStream.of(1,2,3);
+        intStream = IntStream.of(1, 2, 3);
         System.out.println(this.getAverage(intStream));
+
+        intStream = IntStream.of(1, 2, 3);
+        System.out.println(intStreamToString(intStream));
+
+        intStream = IntStream.of(1, 2, 3);
+        System.out.println(intStreamToArray(intStream));
 
     }
 
-    private int getSum(IntStream intStream){
+    private int getSum(IntStream intStream) {
         return intStream.sum();
     }
 
-    private double getAverage(IntStream intStream){
+    private double getAverage(IntStream intStream) {
         OptionalDouble average = intStream.average();
-        if(average.isPresent()){
-             return average.getAsDouble();
+        if (average.isPresent()) {
+            return average.getAsDouble();
         }
 
         return 0;
+    }
+
+    private String intStreamToString(IntStream intStream) {
+        //[1, 2, 3]
+        return intStream.mapToObj(String::valueOf).collect(Collectors.joining(", ", "[", "]"));
+
+    }
+
+    private int[] intStreamToArray(IntStream intStream) {
+        return intStream.toArray();
     }
 
     public static void main(String[] args) {
