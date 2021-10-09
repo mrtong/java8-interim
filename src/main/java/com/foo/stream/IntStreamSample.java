da5b9425eea28c80b3fe25d9b5492d7672088369
mrtong <yanjun.tong@gmail.com>
2021-10-09 14:35:10 +1100

    add more methods in IntStreamSample

--- src/main/java/com/foo/stream/IntStreamSample.java
+++ src/main/java/com/foo/stream/IntStreamSample.java
@@ -1,22 +1,29 @@
 package com.foo.stream;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class IntStreamSample {
     public IntStreamSample() {
         IntStream intStream = IntStream.of(1, 2, 3);
 
-        System.out.println(this.getSum(intStream));
-        intStream = IntStream.of(1, 2, 3);
-        System.out.println(this.getAverage(intStream));
+        System.out.println("sum of intstream is " + this.getSum(intStream));
 
         intStream = IntStream.of(1, 2, 3);
-        System.out.println(intStreamToString(intStream));
+        System.out.println("aveage of intstream is " + this.getAverage(intStream));
 
         intStream = IntStream.of(1, 2, 3);
-        System.out.println(intStreamToArray(intStream));
+        System.out.println("Convert int stream to a string. The result is " + intStreamToString(intStream));
+
+        intStream = IntStream.of(1, 2, 3);
+        System.out.println("Convert int stream to an array. The result is " + intStreamToArray(intStream));
+
+        sumStringStream();
+        convertFromStringStreamToIntList();
 
     }
 
@@ -41,6 +48,25 @@ public class IntStreamSample {
 
     private int[] intStreamToArray(IntStream intStream) {
         return intStream.toArray();
+    }
+
+    private void sumStringStream() {
+        Optional<Integer> reduce = Stream.of("2", "3", "4")
+                .map(Integer::valueOf)
+                .reduce((partial, e) -> {
+                    partial = partial + e;
+                    return partial;
+                });
+        //9
+        System.out.println("sumStringStream = " + reduce.get());
+    }
+
+    private void convertFromStringStreamToIntList() {
+        List<Integer> collect = Stream.of("2", "3", "4")
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+        //[2, 3, 4]
+        System.out.println("convertFromStringStreamToIntList = " + collect);
     }
 
     public static void main(String[] args) {
